2acaf1de3f06e20a226e3e5c5abfc6453a54b898
Alice Grosch <a.grosch@fz-juelich.de>
2022-02-21 18:30:16 +0100

    Update and restructure template

--- share/2.1.1/static/default/js/home-servers.js
+++ share/2.1.1/static/default/js/home-servers.js
@@ -47,44 +47,6 @@ require(["jquery", "jhapi", "utils"], function (
 
 
   /*
-  Reopen selected tabs
-  */
-  $(document).ready(function () {
-    $('.left-tabs > li.nav-item').on('shown.bs.tab', function () {
-      var should_show = JSON.parse(localStorage.getItem("homeTabs"));
-      if (!should_show) should_show = [];
-      var id = $(this).children().first().attr('id');
-      if (!(should_show.includes(id))) should_show.push(id);
-      localStorage.setItem("homeTabs", JSON.stringify(should_show));
-    });
-
-    $('.left-tabs > li.nav-item').on('hidden.bs.tab', function () {
-      var should_show = JSON.parse(localStorage.getItem("homeTabs"));
-      if (!should_show) return;
-      var id = $(this).children().first().attr('id');
-      if (should_show.includes(id)) {
-        should_show.splice(should_show.indexOf(id), 1);
-      }
-      localStorage.setItem("homeTabs", JSON.stringify(should_show));
-    });
-
-    // Reopen open collapses on page reload
-    var tabs = JSON.parse(localStorage.getItem("homeTabs"));
-    if (tabs) {
-      for (const item of tabs) {
-        var tabEl = $('#' + item);
-        if (tabEl.length) {
-          var tab = new bootstrap.Tab(tabEl);
-          tab.show();
-        }
-      }
-    }
-
-    // console.log(JSON.parse(localStorage.getItem("homeTabs")))
-  })
-
-
-  /*
   Callbacks to handle button clicks
   */
 
@@ -418,6 +380,7 @@ require(["jquery", "jhapi", "utils"], function (
 
   $(".save").click(saveChanges);
   $(".reset").click(revertChanges);
+  
   // Check if there are changes and thus if the save and revert buttons should be enabled
   $("select, input").change(function () {
     var that = $(this);
@@ -448,116 +411,6 @@ require(["jquery", "jhapi", "utils"], function (
       }
     }
   })
-
-
-  /*
-  Interface functions
-  */
-  // Check if warning badge whould be shown in tab
-  $("[id*=tab-warning]").on("change", function () {
-    var tab_warning = $(this);
-    var tab = tab_warning.parent();
-    var tab_content = $(tab.data("bsTarget"));
-    var badges = tab_content.find(".badge");
-    var should_show = false;
-
-    badges.each(function () {
-      if ($(this).css("display") != "none") should_show = true;
-    })
-
-    if (should_show && !tab.hasClass("disabled")) {
-      tab_warning.show();
-    } else {
-      tab_warning.hide();
-    }
-  });
-
-  // Remove warning icons when clicking on inputs
-  function onFocus(select, tab_name, setting = "select") {
-    var id = select.attr("id").split('-')[0];
-    var warning_id = select.attr("id").replace("-" + setting, "-warning");
-    select.removeClass("border-warning");
-    $("#" + warning_id).hide();
-    $("#" + id + "-" + tab_name + "-tab-warning")[0].dispatchEvent(new Event("change"));
-  }
-
-  $("div[id*=service] input").on("input", function () {
-    onFocus($(this), "service", setting = "input");
-  });
-
-  $("div[id*=service] select").focus(function () {
-    onFocus($(this), "service");
-  });
-
-  $("div[id*=options] select").focus(function () {
-    onFocus($(this), "options");
-  });
-
-  $("div[id*=resources] input").focus(function () {
-    onFocus($(this), "resources", setting = "input");
-  });
-
-  $("div[id*=reservation] select").focus(function () {
-    onFocus($(this), "reservation");
-  });
-
-  // Check on click if we can remove the warning badge in tab
-  $("[id$=tab").on("click", function () {
-    var tab = $(this);
-    var tab_content = $(tab.data("bsTarget"));
-    var badges = tab_content.find(".badge");
-
-    badges.each(function () {
-      var parent_div = $(this).parents("div").first();
-      // There might be a warning because an existing value does not exist anymore
-      // In that case, the warning badge will be displayed, but not the parent div
-      if ($(this).css("display") != "none" && parent_div.css("display") == "none") {
-        $(this).hide(); // We manually hide the warning badge once the tab has been clicked
-      }
-    })
-    // Send change event to the tab warning to see if we should still show the warning in the tab
-    var tab_warning = tab.find("[id*=warning");
-    tab_warning[0].dispatchEvent(new Event("change"));
-  });
-
-  // Toggle the collapse on table row click
-  $("tr[data-server-name]").not(".progress-tr").not(".collapse-tr").on("click", function () {
-    var collapse = $(this).next().find(".collapse");
-    var first_td = $(this).children().first();
-    var icon = first_td.children().first();
-    var hidden = collapse.css("display") == "none" ? true : false;
-
-    if (hidden) {
-      icon.removeClass("collapsed");
-      new bootstrap.Collapse(collapse);
-    } else {
-      icon.addClass("collapsed");
-      new bootstrap.Collapse(collapse);
-    }
-  });
-  // But not on click of the action td
-  $(".actions-td").on("click", function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-  })
-
-  // Change to log vertical tag on toggle logs
-  // $(".btn[id*=progress-log-btn]").on("click", function(event) {
-  $(".progress-log-btn").on("click", function (event) {
-    var tr = $(this).parents("tr");
-    var collapse = tr.next().find(".collapse");
-    var hidden = collapse.css("display") == "none" ? true : false;
-    var name = tr.data("server-name");
-    // Do not hide collapse if already open, but not showing the logs tab
-    if (!hidden && !$("#" + name + "-logs-tab").hasClass("active")) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    // Change to log vertical tag
-    var trigger = $("#" + name + "-logs-tab");
-    var tab = new bootstrap.Tab(trigger);
-    tab.show();
-  });
 
 
   /*
@@ -673,15 +526,4 @@ require(["jquery", "jhapi", "utils"], function (
   // Handle EventSource message
   // function onEvtMessage(event, evtSource, progress_bar, progress_log) {
   // }
-
-  function askForNotificationPermission() {
-    // Let's check if the browser supports notifications
-    // if (!("Notification" in window)) {
-    //   alert("This browser does not support desktop notification");
-    // }
-    // Ask the user for permission
-    if (Notification.permission !== 'denied') {
-      Notification.requestPermission();
-    }
-  }
 });
