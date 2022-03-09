d1a04b0216545825c954dd56a8351a5a46feb898
Alice Grosch <a.grosch@fz-juelich.de>
2022-03-09 16:59:39 +0100

    Handle server limits from custom config

---
+++ custom/2.1.1/custom_utils/server_limit.py
@@ -0,0 +1,31 @@
+def named_server_limit_per_user_exceeded(user, options):
+    custom_config = user.authenticator.custom_config
+    named_spawners = list(user.all_spawners(include_default=False))
+
+    vo = options["vo"]
+    service = options["service"]
+    service_type = service.split('/')[0]
+    service_option = service.split('/')[1]
+    system = options["system"]
+
+    vo_config = custom_config.get("vos").get(vo)
+
+    service_limit = vo_config.get("Services").get(service_type).get(service_option).get("max_per_user")
+    if service_limit:
+        current = 0
+        for spawner in named_spawners:
+            if spawner.user_options.get("service", "") == service:
+                current += 1
+        if current >= service_limit:
+            return "service", service, service_limit
+
+    system_limit = vo_config.get("Systems", {}).get("max_per_user", {}).get(system)
+    if system_limit:
+        current = 0
+        for spawner in named_spawners:
+            if spawner.user_options.get("system", "") == system:
+                current += 1
+        if current >= system_limit:
+            return "system", system, system_limit
+
+    return
