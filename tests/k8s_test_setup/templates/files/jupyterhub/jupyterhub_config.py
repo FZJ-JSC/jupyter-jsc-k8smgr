733aa72e6459a58fd07d3844dcfd671c9525398a
Alice Grosch <a.grosch@fz-juelich.de>
2022-03-09 14:54:25 +0100

    Remove SpawnCancelAPIHandler

--- tests/k8s_test_setup/templates/files/jupyterhub/jupyterhub_config.py
+++ tests/k8s_test_setup/templates/files/jupyterhub/jupyterhub_config.py
@@ -8,7 +8,6 @@ custom_path = "/src/jupyterhub-custom"
 sys.path.insert(1, custom_path)
 
 from spawner import BackendSpawner
-from apihandler import SpawnCancelAPIHandler
 from apihandler import SpawnUpdateOptionsAPIHandler
 from apihandler import SpawnProgressUpdateAPIHandler, SpawnProgressStatusAPIHandler
 from apihandler import user_cancel_message
@@ -90,8 +89,6 @@ c.JupyterHub.extra_handlers = [
     (r"/groups", page_handlers.VOHandler),
     (r"/signout", BackendLogoutHandler),
     # APIHandlers
-    (r"/api/users/([^/]+)/servers/([^/]*)/cancel", SpawnCancelAPIHandler),
-    (r"/api/users/([^/]+)/server/cancel", SpawnCancelAPIHandler),
     (r"/api/users/([^/]+)/server/update", SpawnUpdateOptionsAPIHandler),
     (r"/api/users/([^/]+)/servers/([^/]*)/update", SpawnUpdateOptionsAPIHandler),
     (r"/api/users/progress/update/([^/]+)", SpawnProgressUpdateAPIHandler),
