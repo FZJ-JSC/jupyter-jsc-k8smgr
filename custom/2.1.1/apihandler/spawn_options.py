4893605cfaf47cc860ebbc2d6a067f83cd70ef39
Alice Grosch <a.grosch@fz-juelich.de>
2022-06-09 12:46:40 +0200

    Add debug messages to spawn options update

--- custom/2.1.1/apihandler/spawn_options.py
+++ custom/2.1.1/apihandler/spawn_options.py
@@ -10,20 +10,22 @@ class SpawnUpdateOptionsAPIHandler(APIHandler):
         user = self.find_user(name)
 
         self.log.debug(f"Update options user: {user}")
-        self.log.debug(f"Update options spawners: {user.spawners}")
         self.log.debug(f"Update options server_name in spawners: {server_name}")
         self.log.debug(server_name not in user.spawners)
 
         if user is None:
             # no such user
+            self.log.debug("Returning 404 user not found")
             raise web.HTTPError(404)
         if server_name not in user.spawners:
             # user has no such server
+            self.log.debug("Returning 404 no such server")
             raise web.HTTPError(404)
         orm_user = user.orm_user
         spawner = orm_user.orm_spawners[server_name]
         # Save new options
         formdata = self.get_json_body()
+        self.log.dedub(f"Update options user: {formdata}")
         try:
             check_formdata_keys(formdata, user.authenticator.custom_config)
         except KeyError as err:
