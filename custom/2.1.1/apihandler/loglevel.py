e82c99e5cfab5ffd108fa9f76e3b4222dafd91fd
Alice Grosch <a.grosch@fz-juelich.de>
2022-07-19 11:53:45 +0200

    Debug logging

--- custom/2.1.1/apihandler/loglevel.py
+++ custom/2.1.1/apihandler/loglevel.py
@@ -181,6 +181,7 @@ class DRFServiceLogLevelAPIHandler(APIHandler):
         log_url = req_prop.get("urls", {}).get("logs", "None")
         if handler:
             log_url = log_url + handler + "/"
+        self.log.debug(f"log_url: {log_url}")
         req = HTTPRequest(
             log_url ,
             method=method,
